67641b9a7fbce2d0475ad1b40e030e03965a8369
Theo Hronowsky <theohronowsky@LABS.SCE.CARLETON.CA>
2018-04-10 21:04:06 -0400

    commenting of code

--- Iteration5/src/ServerFiles/ClientConnection.java
+++ Iteration5/src/ServerFiles/ClientConnection.java
@@ -8,25 +8,30 @@ import java.util.Arrays;
 
 import static java.lang.System.*;
 
+/*
+this is the threadable client class used to handle requests. This class is started by the server
+
+ */
+
 public class ClientConnection extends Thread {
 
-	private DatagramPacket request;
-	private DatagramSocket socket;
+	private DatagramPacket request; //datagram packet to contain the request
+	private DatagramSocket socket; //socket to receive the request
 	private byte[] block = {0,0};
 	private FileOutputStream fileWriter;
-	private static FileInputStream myInputStream;
+	private static FileInputStream myInputStream; // input stream to handle file I/O
 	private static DatagramPacket lastPacket;
 	private static int port;
-	private static final int TIMEOUT = 100;
-	private static final String ServPath = ".\\src\\Serv\\";
-	private static int REC_PORT = 25;
+	private static final int TIMEOUT = 100; //final int for the timeout amount
+	private static final String ServPath = ".\\src\\Serv\\"; //path descriptor for the server
+	private static int REC_PORT = 25; //set the receiving port
 	private static InetAddress localhost;
 
 	private static String type = "";
 	private static InetAddress address;
 
 
-	ClientConnection(DatagramPacket request){
+	ClientConnection(DatagramPacket request){ //constructor to initialize the class
 
 		this.request = request;
 		address = request.getAddress();
@@ -42,7 +47,7 @@ public class ClientConnection extends Thread {
 
 		try {
 			socket = new DatagramSocket(random);
-			socket.setSoTimeout(TIMEOUT);
+			socket.setSoTimeout(TIMEOUT); //set the timeout amount
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
@@ -52,70 +57,70 @@ public class ClientConnection extends Thread {
 
 		out.println("ClientConnection running");
 
-		String type = validatePacket(request);
+		String type = validatePacket(request); //validate the received packet
 		String file = null;
 
 		try {
-			file = getFilename(request);
+			file = getFilename(request); //get the file name from the packet
 		} catch (FileNotFoundException e) {
-			sendError(1, port);
+			sendError(1, port); //raise an error code 1
 			e.printStackTrace();
 		}
-		String mode = getMode(request, file);
+		String mode = getMode(request, file); //determine the mode of the request
 
-		if(!mode.equalsIgnoreCase("octet")) {
+		if(!mode.equalsIgnoreCase("octet")) { //if the mode is not implemented(octet) send an error code 4
 			sendError(4, port);
-			shutdown();
+			shutdown(); //shut down the client connection
 		}
 
-		file = encodeFilename(file);
+		file = encodeFilename(file); //encode the filename
 
 		try {
 			switch (type) {
-				case "WRQ":
+				case "WRQ": //if the type returned is write request, send an ack and print the type
 					sendACK(request);
 					out.println("writeRequest");
 					writeRequest(file);
 					break;
-				case "RRQ":
+				case "RRQ": //if the type returned is read request, send an ack and print the type
 					out.println("Read Request");
 					readRequest(file);
 					break;
-				default:
+				default: //if it is not read or write it is an error and the client connection is shut down
 					shutdown();
 					break;
 			}
 		} catch (IOException e) {
-			String msg = e.getMessage();
+			String msg = e.getMessage(); //get the error message
 
 			if (msg.equals("There is not enough space on the disk") || msg.equals("Not enough space") || msg.equals("No space left on device")) {
-				sendError(3, port);
+				sendError(3, port); //send a error code 3 if there is no room on disk
 			} else if (e.getClass().equals(AccessDeniedException.class)) {
-				sendError(2, port);
+				sendError(2, port); //send a error code 2 if the permissions are not correct
 			} else if (e.getClass().equals(FileAlreadyExistsException.class)) {
-				sendError(6, port);
+				sendError(6, port); //send a error code 6 if the file already exists
 			}
 			e.printStackTrace();
 		}
 	}
 
-	private String validatePacket(DatagramPacket packet)
+	private String validatePacket(DatagramPacket packet) //class to determine what is contained within the packet
 	{
 
 		byte[] data = packet.getData();
 		if(data[0]==0){
 			if(data[1]==1){
-				return "RRQ";
+				return "RRQ"; //it is a read request
 			}else if(data[1]==2){
-				return "WRQ";
+				return "WRQ"; //it is a write request
 			}else if(data[1]==3){
-				return "DATA";
+				return "DATA"; //it is a data packet
 			}else if(data[1]==4){
-				return "ACK";
+				return "ACK"; //it is a acknowledgement
 			}else if(data[1]==5){
-				return "ERROR";
+				return "ERROR"; //it is an error
 			}else{
-				return "INVALID";
+				return "INVALID"; //otherwise it is invalid
 			}
 		}else{
 			return "INVALID";
@@ -123,7 +128,7 @@ public class ClientConnection extends Thread {
 
 	}
 	
-	private void readRequest(String filename) throws IOException{
+	private void readRequest(String filename) throws IOException{ //class to send a read request
 		byte data[] = new byte[512];
 		DatagramPacket received = new DatagramPacket(data, data.length);
 		byte[] fileBytes;
@@ -198,10 +203,10 @@ public class ClientConnection extends Thread {
 	}
 
 
-	private void sendError(int code, int port)
+	private void sendError(int code, int port) //class to send error based on an error code
 	{
 		String errorMessage;
-		byte[] data;
+		byte[] data; //byte array to contain an error code
 		switch(code){
 			case 0: errorMessage =	"UNKNOWN ERROR";
 			break;
@@ -228,7 +233,7 @@ public class ClientConnection extends Thread {
 		data[1] = 5;
 		data[2] = 0;
 		data[3] = (byte) code;
-		out.println(errorMessage);
+		out.println(errorMessage); //prints out the concatonated error message
 
 		arraycopy(message, 0, data, 4, message.length + 4 - 4);
 		data[data.length-1] = 0;
@@ -236,7 +241,7 @@ public class ClientConnection extends Thread {
 		lastPacket = send;
 
 		try {
-			socket.send(send);
+			socket.send(send); //send the error message/code
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -255,9 +260,9 @@ public class ClientConnection extends Thread {
 
 	private String encodeFilename(String filename){
 		return ServPath + filename;
-	}
+	} //encode the filename by concatonating the path with the filename
 
-	private void writeRequest(String filename) throws IOException{
+	private void writeRequest(String filename) throws IOException{ //class to send a write request
 		byte data[] = new byte[512];
 		DatagramPacket received = new DatagramPacket(data, data.length);
 		boolean keepReceiving = true;
@@ -283,18 +288,18 @@ public class ClientConnection extends Thread {
                             byte[] blockNumber = unpackBlockNumber(received);
 
                             if (Arrays.equals(blockNumber, nextBlock(block))) {
-                                if (received.getLength() < 512) {
+                                if (received.getLength() < 512) { //if the length is less than 512 it is end of transfer
                                     keepReceiving = false;
                                 }
                                 else if (received.getLength() > 512) {
-									sendError(4,received.getPort()); //if greater than 512, its an inval
+									sendError(4,received.getPort()); //if greater than 512, its an invalid block
 									shutdown();
 								}
                                 block = nextBlock(block);
                                 fileWriter.write(receivedBytes);
                                 sendACK(received);
                             } else if (Arrays.equals(blockNumber, block)) {
-                                sendACK(received);
+                                sendACK(received); //send an ack to acknowledge data has been received
                             } else {
                             	sendError(4, received.getPort());
                             	shutdown();
@@ -305,12 +310,12 @@ public class ClientConnection extends Thread {
                             shutdown();
                         }
                         break;
-                    case "ERROR":
+                    case "ERROR": //case to handle error within the client
                         keepReceiving = false;
                         out.println("Client had an ERROR");
                         shutdown();
                         break;
-					case "INVALID":
+					case "INVALID": //if the opcode is invalid, send error code 4 and shutdown
 						keepReceiving = false;
 						out.println("opcode error");
 						sendError(4, received.getPort());
@@ -318,7 +323,7 @@ public class ClientConnection extends Thread {
 						break;
                     default:
                         keepReceiving = false;
-                        out.println("There was an ERROR");
+                        out.println("There was an ERROR"); //print out an error has occured
                         shutdown();
                         break;
                 }
@@ -326,9 +331,9 @@ public class ClientConnection extends Thread {
 		}
 	}
 	
-	private void sendACK(DatagramPacket packet)
+	private void sendACK(DatagramPacket packet) //class to send an acknowledgment
 	{
-		byte[] data = new byte[4];
+		byte[] data = new byte[4]; //byte to contain the code which denotes an ack
 		data[0] = 0;
 		data[1] = 4;
 		data[2] = block[0];
@@ -343,9 +348,9 @@ public class ClientConnection extends Thread {
 		}
 	}
 
-	private byte[] sendData(byte[] fileBytes, DatagramPacket packet) throws IOException
+	private byte[] sendData(byte[] fileBytes, DatagramPacket packet) throws IOException //class to send a data packet
 	{
-		byte[] data = new byte[512];
+		byte[] data = new byte[512]; //byte tp contain the code which denotes data
 		data[0] = 0;
 		data[1] = 3;
 		data[2] = block[0];
@@ -378,7 +383,7 @@ public class ClientConnection extends Thread {
 	}
 	
 	//Creates the DatagramPacket following the guidelines in the assignment document
-	private byte[] createArray(String filename) {
+	private byte[] createArray(String filename) { //creates an array of the file in bytes
 		out.println(filename);
 		myInputStream = null;
 		File file = new File(filename);
@@ -404,7 +409,7 @@ public class ClientConnection extends Thread {
 		return fileBytes;
 	}
 	
-	private String getFilename(DatagramPacket receivedPacket) throws FileNotFoundException
+	private String getFilename(DatagramPacket receivedPacket) throws FileNotFoundException //class to obtain the file name contained within the packet
 	{
 		byte[] content = receivedPacket.getData();
 		int len = receivedPacket.getLength();
@@ -423,7 +428,7 @@ public class ClientConnection extends Thread {
 
 	}
 
-	private String getMode(DatagramPacket receivedPacket, String filename)
+	private String getMode(DatagramPacket receivedPacket, String filename) //calss to setermine which mode is within the packet
 	{
 		byte[] content = receivedPacket.getData();
 		int len = receivedPacket.getLength();
@@ -443,7 +448,7 @@ public class ClientConnection extends Thread {
 
 	}
 
-	private byte[] unpackBlockNumber(DatagramPacket packet)
+	private byte[] unpackBlockNumber(DatagramPacket packet) //class to unpack the block number of the packet
 	{
 		byte[] packetData = packet.getData();
 		byte[] data = new byte[2];
@@ -453,7 +458,7 @@ public class ClientConnection extends Thread {
 		return data;
 	}
 
-	private byte[] nextBlock(byte[] myBlock)
+	private byte[] nextBlock(byte[] myBlock) //determine the next block within the packet
 	{
 		byte[] ret = new byte[2];
 		if(myBlock[1] < 127){
@@ -466,7 +471,7 @@ public class ClientConnection extends Thread {
 		return ret;
 	}
 
-	private void shutdown() {
+	private void shutdown() { //class to shut down the client connection
 		socket.close();
 		try {
 			fileWriter.close();
--- Iteration5/src/ServerFiles/Server.java
+++ Iteration5/src/ServerFiles/Server.java
@@ -6,25 +6,26 @@ import java.util.*;
 
 
 import static java.lang.System.*;
-
-
+/*This class is the Server class which creates client connection threads when a request is received
+SYSC 3303 - Group 8
+*/
 public  class Server {
 
-	private static DatagramSocket recSocket;
-	private static DatagramPacket receivedPacket;
-	private static final int TIMEOUT = 0;
+	private static DatagramSocket recSocket; //socket to receive transmissions
+	private static DatagramPacket receivedPacket; //receiving packet to contain the data
+	private static final int TIMEOUT = 0; //final integer to contain the timout time
 	private static String type = "";
 	private static Scanner input1;
 
 	public static void main(String[] args){
 
 		try {
-			out.println(InetAddress.getLocalHost());
+			out.println(InetAddress.getLocalHost()); //prints out the local address that the server is running on in order to easily access this adress
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 
-		byte[] data = new byte[512];
+		byte[] data = new byte[512]; //create a new byte array to store the received datagram packet
 		receivedPacket = new DatagramPacket(data, data.length);
 		Scanner scanner = new Scanner(in);
 		boolean cont = true;
@@ -32,10 +33,10 @@ public  class Server {
 
 		
 		try {
-			recSocket = new DatagramSocket(69);
-			recSocket.setSoTimeout(TIMEOUT);
+			recSocket = new DatagramSocket(69); //set the port number to receive from
+			recSocket.setSoTimeout(TIMEOUT); //set the timeout amount for the receiving socket
 		} catch (SocketException e) {
-			e.printStackTrace();
+			e.printStackTrace(); //error handling
 			exit(1);
 		}
 
@@ -43,23 +44,23 @@ public  class Server {
 
 		while(true){
 			try {
-				recSocket.receive(receivedPacket);
+				recSocket.receive(receivedPacket); //receive the datagram packet on the receiver socket
 				System.out.println("Received Packet");
 				cont = true;
 			} catch (IOException e) {
 				if (e.getClass().equals(SocketTimeoutException.class)) {
 					cont = false;
 				} else {
-					e.printStackTrace();
+					e.printStackTrace(); //error handling
 				}
 			}
 			if(cont) {
-				new ClientConnection(receivedPacket).start();
+				new ClientConnection(receivedPacket).start(); //creates a new client connection thread to handle the request
 			}
 			if (scanner.hasNext()){
-				String input = scanner.next();
+				String input = scanner.next(); //access the keyboard input
 
-				if(input.equalsIgnoreCase("q")){
+				if(input.equalsIgnoreCase("q")){ //if q or Q is entered, the server is shutdown
 		    	  out.println("user has terminated the server. Shutting down");
 		    	  exit(0);
 				}
